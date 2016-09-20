b296a153d5eae001fdcf7d8ed0671ebed22af457
Nick Meharry <nick@drchrono.com>
2016-09-20 01:29:53 -0700

    Add missing merge migration

---
+++ oauth2_provider/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oauth2_provider', '0003_auto_20160316_1503'),
+        ('oauth2_provider', '0003_increase_redirect_uri'),
+    ]
+
+    operations = [
+    ]
